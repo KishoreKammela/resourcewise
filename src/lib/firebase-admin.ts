5eec5f6e11b9f4b69ba888712369dfc3071fd61a
kishore kammela <kishorekammela@gmail.com>
2025-07-26 09:56:19 +0000

    update env decoding

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,9 +2,27 @@ import 'dotenv/config';
 import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(
-    process.env.NEXT_FIREBASE_SERVICE_ACCOUNT_KEY as string
-  );
+  const serviceAccountKey = process.env.NEXT_FIREBASE_SERVICE_ACCOUNT_KEY;
+
+  if (!serviceAccountKey) {
+    throw new Error(
+      'Firebase service account key is not set in environment variables.'
+    );
+  }
+
+  let serviceAccount;
+  // Check if the key is a JSON string or Base64 encoded
+  if (serviceAccountKey.startsWith('{')) {
+    // It's a JSON string (likely from local .env file)
+    serviceAccount = JSON.parse(serviceAccountKey);
+  } else {
+    // It's likely Base64 encoded (for production environments like Vercel)
+    const decodedKey = Buffer.from(serviceAccountKey, 'base64').toString(
+      'utf-8'
+    );
+    serviceAccount = JSON.parse(decodedKey);
+  }
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
